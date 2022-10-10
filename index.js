624394211a03ab722a0bf698037800f6c1fd133e
oamr <or.deeb@gmail.com>
2022-10-10 12:07:42 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
+let random = Math.ceil(Math.random());
+let num3 =16 ;
+let num4 = 6 ;
+let mod = num3 % num4;
+let max;
+max = Math.max(5, 10, 15, 20);
 // Write your code here
